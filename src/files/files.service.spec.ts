933a03cb301bb890420e15d01b369ab99e68910d
Edwin Reule <edwinreule@gmail.com>
2025-07-04 04:32:42 -0300

    feat: add S3 presigned URL generation

--- src/files/files.service.spec.ts
+++ src/files/files.service.spec.ts
@@ -15,4 +15,13 @@ describe('FilesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should generate a presigned url', async () => {
+    process.env.AWS_ACCESS_BUCKET = 'bucket';
+    process.env.AWS_REGION = 'us-east-1';
+    process.env.AWS_ACCESS_KEY_ID = 'key';
+    process.env.AWS_SECRET_ACCESS_KEY = 'secret';
+    const url = await service.generatePresignedUrl('folder', 'file', 60);
+    expect(typeof url).toBe('string');
+  });
 });
